3fae6b75ad2c50d900129cc9046e1faffbb49941
Vojtěch Michal <vojta.michall@email.cz>
2021-03-11 13:07:42 +0100

    Minor adjustments to protocol 1

--- ADC/getADCreadingFrom.m
+++ ADC/getADCreadingFrom.m
@@ -1,6 +1,9 @@
 function [word] = getADCreadingFrom(filePath)
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
+% Souèást øešení domácího úkolu èíslo 1 z pøedmìtu SME v letním semestru 2021 na FEL ÈVUT v Praze.
+% (Protokol z mìøení napìtí AD pøevodníky).
+% Interprets the specified file as measurement log and extracts all
+% measured ADC words. Their values is then averaged to handle edge cases
+% when the converter would tend to oscillate around 1 lsb.
 data = readmatrix(filePath);
 
 isStable = data(:, 1) == 0;
